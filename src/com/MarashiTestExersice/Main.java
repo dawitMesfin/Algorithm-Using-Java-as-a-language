dc5d9b2f24cd303279922b92f125ebe3e5ea3426
Dawit Mesfin <dawitmesfin2019@gmail.com>
2021-05-17 11:19:30 +0300

    First commit

---
+++ src/com/MarashiTestExersice/Main.java
@@ -0,0 +1,12 @@
+package com.MarashiTestExersice;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+         String x = "Hello mr java ";
+
+          System.out.println(x);
+        }
+}
