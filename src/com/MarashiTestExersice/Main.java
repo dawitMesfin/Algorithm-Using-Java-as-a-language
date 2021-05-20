ecd0ead9f3d44b640f5423f8bcd73c766b3ffbc0
Dawit Mesfin <dawitmesfin2019@gmail.com>
2021-05-20 10:18:10 +0300

    Updated with isMagic

--- src/com/MarashiTestExersice/Main.java
+++ src/com/MarashiTestExersice/Main.java
@@ -1,8 +1,12 @@
 package com.MarashiTestExersice;
 
+import javax.sound.midi.Soundbank;
+import java.sql.Array;
+import java.sql.SQLOutput;
+
 public class Main {
 
-    //Question2.The sum of each digit in a given number?
+    //Question1.The sum of each digit in a given number?
      public static int sumOFDigits(int n){
 
          int sum = 0;
@@ -18,12 +22,76 @@ public class Main {
      }
 
 
+     // Question2 Is a given number prime?
 
-       public static void main(String[] args) {
+      public  static int isPrime(int n){
+         boolean flag = true;
+         for(int i = 2; i <= n/2; i++){
+             if(n % i == 0){
+                 flag = false;
+                 break;
+             }
+         }
+         if (flag){
+             return 1;
+         }
+         return 0;
+      }
 
-           int result = sumOFDigits(127);
-           System.out.println(result);
+     //Question3 Number of prime numbers in a given range m and n
+
+    public  static int numberOfPrimes(int m , int n){
+         int count = 0;
+         for(int i = m + 1; i < n; i++){
+             boolean flaging = true;
+
+             for (int j = 2; j < i/2; j ++){
+                   if (i%j==0){
+                       flaging = false;
+                       break;
+                   }
+               }
+               if (flaging){
+                   count = count +1;
+               }
+           }
+         return count;
+    }
+
+    // Question4 Is the given array Magic Array?
+
+    public static int isMagic(int [] givenArray){
+         int firstElement = givenArray[0];
+         int primSum = 0;
+         for (int i = 0; i < givenArray.length; i++){
+
+               boolean isPrime = true;
+               for (int j = 2; j < givenArray[i]; j++){
+
+                     if(givenArray[i] % j == 0){
+
+                         isPrime = false;
+                         break;
+                     }
+               }
+               if (isPrime && givenArray[i] >= 0) {
+                   primSum = primSum + givenArray[i];
+               }
 
 
+         }
+         if (primSum == firstElement){
+             return 1;
+
+         }
+         return 0;
+    }
+
+
+
+      public static void main(String[] args) {
+         int [] array = {26,13,2,5,3,3};
+      int result = isMagic(array);
+          System.out.println(result);
     }}
 
