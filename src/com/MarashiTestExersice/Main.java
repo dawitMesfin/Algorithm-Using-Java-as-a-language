62f3b920f5aced33e3618e4b1fef0586e016682c
Dawit Mesfin <dawitmesfin2019@gmail.com>
2021-06-02 12:39:49 +0300

    updated june/2/2021 wedensday

--- src/com/MarashiTestExersice/Main.java
+++ src/com/MarashiTestExersice/Main.java
@@ -531,15 +531,348 @@ public class Main {
         return 0;
     }
 
+  // Question 19 is there prime and num one in the array?
+
+  public static int isBunker3(int [] a){
+        for (int i = 0; i < a.length; i ++){
+            boolean isPrime = true;
+            for (int j = 2; j < a[i]/2; j ++){
+                if (a[i] %  j == 0){
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime){
+                for (int k = 0; k < a.length; k ++){
+                    if (a[k] == 1){
+                        return 1;
+                    }
+                }
+            }
+        }
+        return 0;
+  }
+
+  // Using isPrime method
+
+    public static int isBunker4(int [] a){
+        for (int i = 0; i < a.length; i ++){
+            if (isPrime(a[i]) == 1){
+                for (int j = 0; j < a.length; j ++){
+                    if (a[j] == 1){
+                        return 1;
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+
+  // Question 20 are n + 1 and n - 1 in the array?
+
+  public static int isNice(int [] a){
+
+      for (int i = 0; i < a.length; i++){
+          int num = a[i];
+          boolean hasIncOrDecByOne = false;
+
+          for (int j = 0; j < a.length; j++){
+              if (num == a[j] + 1 || num == a[j] - 1) {
+                  hasIncOrDecByOne = true;
+                  break;
+              }
+          }
+
+          if (!hasIncOrDecByOne) return 0;
+      }
+      return 1;
+  }
+
+
+  // Question 21 does a given number have a continuous factor?
+
+    public static int isContinous (int num) {
+        for(int i = 2; i <= num/2; i++) {
+            if (num % i == 0) {
+                int result = i;
+                for (int j = i + 1; j <= num/2; j++) {
+                    if (num % j != 0) break;
+                    result = result * j;
+                    if (result == num) return 1;
+                }
+            }
+        }
+        return 0;
+    }
+
+  // isContiniosFactored
+
+    public static int isContiniousFacotred(int n){
+        int result = 0;
+        for (int i = 2; i <= n/2; i++){
+            if (n % i == 0){
+                result = i;
+                for (int j = i + 1; j <= n/2; j++){
+                    if (n % j != 0){
+                        break;
+                    }
+                    result = result * j;
+                    if (result == n){
+                        return 1;
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+
+
+  // Question 22 is every prime num in the array with its twin?
+
+
+  public static int isTwin(int [] a){
+        for (int i = 0; i < a.length; i++){
+            int outerNum = a[i];
+            if (isPrime(a[i]) == 1){
+                boolean twinFlag = false;
+                for (int j = 0; j < a.length; j++){
+                    int innerNum = a[j];
+                    if (isPrime(innerNum) == 1  && a[i] != a[j]){
+                        if (innerNum - outerNum == 2 || outerNum - innerNum == 2) {
+                            twinFlag = true;
+
+                            System.out.println(a[i] + " " + a[j] + " " + twinFlag);
+                            break;
+                        }
+                    }
+                }
+
+                if (!twinFlag) return 0;
+            }
+        }
+        return 1;
+  }
+
+  // Question 23 is every even number in the array has its own double?
+
+public static int isDoubled(int [] arr){
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i] % 2 ==0 ) {
+                boolean doubleFlag = false;
+                for (int j = 0; j < arr.length; j++){
+                    if (2 * arr[i] == arr[j]) {
+                        doubleFlag = true;
+                        break;
+                    }
+                }
+
+                if (doubleFlag) {
+                    return 1;
+                }
+            }
+        }
+        return 0;
+    }
+ // Question 24 do every element fond in a array in b array?
+
+ public static int isSetEqual(int [] a , int [] b){
+        for (int i = 0; i < a.length; i++) {
+            boolean setFalag = false;
+            for (int j = 0; j < b.length; j++) {
+                if (a[i] == b[j]) {
+                    setFalag = true;
+                    break;
+                }
+            }
+            if (!setFalag) {
+                return 0;
+            }
+        }
+
+     for (int k = 0; k < b.length; k++){
+         boolean setFlag2 = false;
+         for (int m = 0; m < a.length; m++){
+             if(b[k] == a[m]){
+                 setFlag2 = true;
+                 break;
+             }
+         }
+
+         if (!setFlag2) return 0;
+     }
+
+     return 1;
+ }
+
+ //Question 25 Does the given n found in a smart squence?
+ public static int isSmart(int num){
+        int sum = 1;
+
+        for(int i = 1; sum <= num; i++) {
+            if (sum == num) return 1;
+            sum += i;
+        }
+
+        return 0;
+ }
+
+ // Question Is nice array?
+
+ public static int isNiceArray(int [] a){
+        int sum = 0;
+        int firstElement = a[0];
+        for (int i = 0; i < a.length; i++){
+            if (isPrime(a[i]) == 1){
+                sum = sum + a[i];
+            }
+        }
+        if (sum == firstElement){
+            return 1;
+        }
+        return 0;
+ }
+
+ // Question 26 isComplete 2;
+
+ public static int  isComplete2(int [] a) {
+     int maxEven = a[0];
+     for (int i = 0; i < a.length; i++) {
+         if (a[i] < 0) {
+             return 0;
+         }
+         if (a[i] % 2 == 0 && maxEven <= a[i]) {
+             maxEven = a[i];
+
+         }}
+         if (maxEven % 2 != 0) {
+             return 0;
+         }
+
+     System.out.println(maxEven);
+     for (int k = 2; k < maxEven; k++){
+         if (k % 2 == 0){
+
+         boolean isPresent = false;
+
+         for (int j = 0; j < a.length; j++){
+             if (k == a[j]){
+                 isPresent = true;
+                 break;
+             }
+
+         }
+         if(!isPresent){
+             return 0;
+         }
+     }}
+
+ return 1;
+ }
+
+// Question 27 are the two numbers factor equal?
+
+    public static int areEqualFactor(int a, int b){
+        int counterA = 0;
+        int counterB = 0;
+        for (int i = 1; i <= a; i++){
+            if (a % i == 0){
+                counterA ++;
+            }
+        }
+        for(int i = 1; i <= b; i++){
+            if (b % i == 0){
+                counterB ++;
+            }
+        }
+          if (counterA == counterB){
+              return 1;
+          }
+          return 0;
+    }
+
+// Question 28 is Merra?
+public static int isMeera(int [] a){
+    int sum = 0;
+
+    for (int i = 0; i < a.length; i++){
+        if (a[i] > i){
+            return 0;
+        }
+    }
+
+    if (sum == 0){
+        return 1;
+    }
+    return 0;
+}
+
+// Question 29 isTriple array?
+
+ public static int isTriple(int [] a){
+        for (int i = 0; i < a.length; i++){
+            int counter = 0;
+            boolean isTripled = false;
+            for (int j = 0; j < a.length; j++){
+                if (a[i] == a[j]){
+                    counter++;
+
+                }
+            }
+            if (counter == 3){
+                isTripled = true;
+            }
+            if (!isTripled){
+                return 0;
+            }
+        }
+        return 1;
+ }
+
+// Question 30 Fibonacci numbers{1,1,2,3,5,8,13,21,34...}
+
+   public  static int isFibonacci(int n){
+        int prevSum = 1;
+        int currentSum = 1;
+        int result = 0;
+        for(int i = 0; i < n; i++){
+            result = prevSum + currentSum;
+            if (result == n){
+                return 1;
+            }
+            prevSum = currentSum;
+            currentSum = result;
+
+
+   }
+    return 0;}
+
+ // Question 31 given n then find the n'th fibonacci number
+
+public static int fibOFNum(int n){
+        int fib1 = 0;
+        int fib2 = 1;
+        int result = 0;
+        for (int i = 0; i <= n; i++){
+            result = fib1 + fib2;
+            fib1 = fib2;
+            fib2 = result;
+        }
+        return result;
+}
+
+// Question 32 isBeanArray?
+
 
 
 
 
         public static void main(String [] args){
-         int [] array = {2,3,9,7};
-         int result = isMeera(1);
-            System.out.println(result);
+        int[] array = { 1, 2, 1, 3, 1, 3, 2, 2} ;
+        int [] array1 = {1,0,2,2,1,0,5};
+        int result = fibOFNum(1000);
+        System.out.println(result);
 
-        }
-    }
+
+}}
+
 
